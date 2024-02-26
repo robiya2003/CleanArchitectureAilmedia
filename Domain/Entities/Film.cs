d4edd3b84c2f37f37a6ce2fdf7e8e350b7917422
Muallimaqiz2003 <robiyahakimova2003@gmail.com>
2024-02-26 07:42:31 +0500

    Add project files.

---
+++ Domain/Entities/Film.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Film:AudiTable
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public long FilmMakerId {  get; set; }
+        public int AgeRestriction { get; set;}
+        public int PublishedYear {  get; set; }
+        public FilmMaker FilmMaker { get; set; }
+        public List<Actor> Actors { get; set; }
+        public  List<Genre> Genres { get; set; }
+    }
+}
---
+++ Domain/Entities/FilmMaker.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Common;
+using Domain.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class FilmMaker:AudiTable
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Description { get; set; }
+        public Gender Gender { get; set; }
+
+        public List<Film> Films { get; set; }
+    }
+}
