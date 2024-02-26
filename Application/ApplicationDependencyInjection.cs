04c0c77e0a1b9bd28b6388e8b2c8ac0b1a934048
Muallimaqiz2003 <robiyahakimova2003@gmail.com>
2024-02-26 09:35:51 +0500

    Appliction Layer bitdi

---
+++ Application/ApplicationDependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Application.Services.Actors;
+using Application.Services.FilmMakers;
+using Application.Services.Films;
+using Application.Services.Genres;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public static class ApplicationDependencyInjection
+    {
+        public static IServiceCollection AddApplicationServises(this IServiceCollection services)
+        {
+            services.AddScoped<IActorServise, ActorServise>();
+            services.AddScoped<IFilmMakerServise, FilmMakerServise>();
+            services.AddScoped<IFilmServise, FilmServise>();
+            services.AddScoped<IGenreServise, GenreServise>();
+            return services;
+        }
+    }
+}
